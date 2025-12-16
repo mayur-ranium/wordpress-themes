8c91d5c9e865dc22474e0a37b2a1004be3bb0bbb
Mayur Chaudhari <mayur.chaudhari@ranium.in>
2025-12-16 17:55:13 +0530

    feat: Add new assets, templates, and update theme settings

---
+++ themes/my-custom-block-theme-new/patterns/mycalltoaction.php
@@ -0,0 +1,31 @@
+<?php 
+/*
+* Title: Call to Action 1
+* Slug: my-custom-block-theme-new/mycalltoaction
+* Categories: call-to-action, another-category
+* Keywords: keyword
+*/
+?>
+
+
+<!-- wp:cover {"url":"https://pd.w.org/2022/01/2661f82cd1ba9172.16386413.jpg","id":334,"dimRatio":90,"customOverlayColor":"#2c3066","isUserOverlayColor":true,"minHeight":50,"minHeightUnit":"vh","align":"full","style":{"spacing":{"padding":{"top":"4vw","right":"4vw","bottom":"4vw","left":"4vw"},"margin":{"top":"0"}},"color":{"duotone":["rgb(44, 48, 102)","#ffb23d"]}}} -->
+<div class="wp-block-cover alignfull" style="margin-top:0;padding-top:4vw;padding-right:4vw;padding-bottom:4vw;padding-left:4vw;min-height:50vh"><img class="wp-block-cover__image-background wp-image-334" alt="" src="https://pd.w.org/2022/01/2661f82cd1ba9172.16386413.jpg" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim-90 has-background-dim" style="background-color:#2c3066"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"blockGap":"0px"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"160px","textTransform":"uppercase","fontStyle":"normal","fontWeight":"700","lineHeight":"0.9","letterSpacing":"-2px"},"color":{"text":"#ffb23d"}}} -->
+<p class="has-text-align-center has-text-color" style="color:#ffb23d;font-size:160px;font-style:normal;font-weight:700;letter-spacing:-2px;line-height:0.9;text-transform:uppercase">Enjoy</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"160px","textTransform":"uppercase","fontStyle":"normal","fontWeight":"700","lineHeight":"0.9","letterSpacing":"-2px"},"color":{"text":"#ffb23d"}}} -->
+<p class="has-text-align-center has-text-color" style="color:#ffb23d;font-size:160px;font-style:normal;font-weight:700;letter-spacing:-2px;line-height:0.9;text-transform:uppercase">20% Off</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"48px"} -->
+<div style="height:48px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"style":{"color":{"background":"#ffb23d","text":"#2c3066"},"spacing":{"padding":{"top":"32px","right":"48px","bottom":"32px","left":"48px"}},"typography":{"fontSize":"20px","textTransform":"uppercase","letterSpacing":"1px","fontStyle":"normal","fontWeight":"700"}}} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-text-color has-background has-custom-font-size wp-element-button" style="color:#2c3066;background-color:#ffb23d;padding-top:32px;padding-right:48px;padding-bottom:32px;padding-left:48px;font-size:20px;font-style:normal;font-weight:700;letter-spacing:1px;text-transform:uppercase">Shop Now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
